53bd2846b4b1d29d52eec008bfa5add3dbf2f330
Jake <jake.bowles@protonmail.com>
2022-10-19 23:19:12 +0100

    aks deployment working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # activity-planner
+kubectl exec --stdin --tty $name -- /bin/bash
+kubectl port-forward deployment/nginx 8000:80
--- src/index.php
+++ src/index.php
@@ -4,17 +4,17 @@
 
 <h1>Pending</h1>
 
-<?php #echo "This is where the magic will go"
+<?php echo "This is where the magic will go"
 
-$conn = mysqli_init();
-mysqli_options($conn,MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
-mysqli_ssl_set($conn,NULL,NULL, "./CA.crt.pem", NULL, NULL); #try and retrieve this from azure key vault
-mysqli_real_connect($conn,"activity-planner-dev.mysql.database.azure.com", "adminuser", "M0Nday123!?***!", "devtest");
-printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM persons');
-while ($row = mysqli_fetch_assoc($res)) {
-var_dump($row);
-}
+// $conn = mysqli_init();
+// mysqli_options($conn,MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+// mysqli_ssl_set($conn,NULL,NULL, "./CA.crt.pem", NULL, NULL); #try and retrieve this from azure key vault
+// mysqli_real_connect($conn,"activity-planner-dev.mysql.database.azure.com", "adminuser", "M0Nday123!?***!", "devtest");
+// printf("Reading data from table: \n");
+// $res = mysqli_query($conn, 'SELECT * FROM persons');
+// while ($row = mysqli_fetch_assoc($res)) {
+// var_dump($row);
+// }
 ?>
 
 </body>
