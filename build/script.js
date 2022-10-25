3ae336ce544029ad1cbbdf4a2789d53c23fa2cdb
jakebowleswpp <jake.bowles@wpp.com>
2022-10-25 13:03:53 +0100

    test log

--- build/script.js
+++ build/script.js
@@ -1,13 +1,14 @@
-fetch("https://catfact.ninja/fact")
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("NETWORK RESPONSE ERROR");
-    }
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch((error) => console.error("FETCH ERROR:", error));
+// fetch("http://catfact.ninja/fact")
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error("NETWORK RESPONSE ERROR");
+//     }
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch((error) => console.error("FETCH ERROR:", error));
 
+console.log("TEST")
