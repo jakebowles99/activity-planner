984e93bd2c3e2bfb7e8738a567a6efe273e1838c
jakebowleswpp <jake.bowles@wpp.com>
2022-10-27 16:20:30 +0100

    move around files

--- build/static/script.js
+++ build/static/script.js
@@ -1,1 +1,12 @@
-console.log("TEST")
+fetch("https://catfact.ninja/fact")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
