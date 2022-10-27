4d0f72e7753ca79a5ec2c93132190e23d2cb0e90
jakebowleswpp <jake.bowles@wpp.com>
2022-10-27 21:12:18 +0100

    new build

---
+++ build/precache-manifest.0706cc3f49720104b0e98d6f6200ca32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "641d801c37d389c81b4c",
+    "url": "/static/css/main.7d73de90.chunk.css"
+  },
+  {
+    "revision": "641d801c37d389c81b4c",
+    "url": "/static/js/main.641d801c.chunk.js"
+  },
+  {
+    "revision": "af0101bce62a9a0a2026",
+    "url": "/static/js/1.af0101bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f9412f9df3379ba228e587ff201cfd7",
+    "url": "/index.html"
+  }
+];
